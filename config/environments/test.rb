d57c118f0e1f880bcfb97c1b938a76f3db84e79b
EC2 Default User <ec2-user@ip-172-31-35-0.us-east-2.compute.internal>
2019-09-23 17:03:13 +0000

    Add account activation

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+  Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'example.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
